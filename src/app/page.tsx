b98c49bbb74dd70ed31170a50950bae1fb77403f
Abhishek Sharma <abhitiku2003@gmail.com>
2024-07-22 22:25:40 +0530

    added signup flow

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import Hero from "@/components/Hero";
+import { Integration } from "@/components/Integration";
 import Layout from "@/components/Layout";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <Layout><Hero /></Layout>
+    <Layout><Hero /><Integration /></Layout>
   );
 }
---
+++ src/lib/appwriteConfig.ts
@@ -0,0 +1,9 @@
+import { Client, Databases, Account } from "appwrite";
+
+const client = new Client();
+client
+  .setEndpoint("https://cloud.appwrite.io/v1")
+  .setProject(process.env.APPWRITE_PROJECT_ID!);
+
+export const account = new Account(client);
+export const database = new Databases(client);
